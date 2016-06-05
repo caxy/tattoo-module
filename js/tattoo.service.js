9c724abe912a803a0a5143d90612a6f72a20b3d3
Benjamin Doherty <bjd@pobox.com>
2016-06-05 11:22:05 -0500

    Only extract entities from head element.

--- js/tattoo.service.js
+++ js/tattoo.service.js
@@ -7,7 +7,7 @@
     TattooService.$inject = ['$document', '_'];
 
     function TattooService($document, _) {
-        var scripts = $document.find('script[type="application/hal+json"][data-drupal-selector="tattoo-entity"]');
+        var scripts = $document.find('head > script[type="application/hal+json"][data-drupal-selector="tattoo-entity"]');
 
         return _.map(scripts, function (script) {
             return JSON.parse(script.textContent);

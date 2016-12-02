1726708aaa6474126c0b67f8bc0da0dba0cc8204
Benjamin Doherty <bjd@bangpound.org>
2016-12-02 11:23:52 -0600

    fix indentation.

--- js/tattoo.js
+++ js/tattoo.js
@@ -3,23 +3,23 @@
  * Parse inline HAL-JSON representations of Drupal entities for tattoo global array.
  */
 (function() {
-    'use strict';
+  'use strict';
 
-    // Use direct child elements to harden against XSS exploits when CSP is on.
-    var scriptElements = document.querySelectorAll('head > script[type="application/hal+json"][data-drupal-selector="tattoo-entity"]');
+  // Use direct child elements to harden against XSS exploits when CSP is on.
+  var scriptElements = document.querySelectorAll('head > script[type="application/hal+json"][data-drupal-selector="tattoo-entity"]');
 
-    /**
-     * Variable generated with all the tattooed entities.
-     *
-     * @global
-     *
-     * @type {object}
-     */
-    window.tattoo = [];
+  /**
+   * Variable generated with all the tattooed entities.
+   *
+   * @global
+   *
+   * @type {object}
+   */
+  window.tattoo = [];
 
-    if (scriptElements !== null) {
-        window.tattoo = _.map(scriptElements, function (script) {
-            return JSON.parse(script.textContent);
-        });
-    }
+  if (scriptElements !== null) {
+    window.tattoo = _.map(scriptElements, function (script) {
+      return JSON.parse(script.textContent);
+    });
+  }
 })();
